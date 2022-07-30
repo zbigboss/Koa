a2e36522ea7bc3d075e69ac9f90631194cecea08
翁俊梓 <1916222162@qq.com>
2022-07-30 16:48:45 +0800

    feat: router迁移

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
 const Koa = require("koa2"); // 构造函数
 const app = new Koa(); // 声明一个实例
 const port = 8089;
+const router = require("./router");
 
-app.use(async (ctx) => {
-  ctx.body = "hello,koa";
-});
+// app.use(async (ctx) => {
+//   ctx.body = "hello,koa";
+// });
+
+/**
+ * use()就是调用router中间件
+ * router.routes()的作用是：启动路由
+ * router.allowedMethods()的作用是：允许任何请求
+ */
+app.use(router.routes(), router.allowedMethods());
 
 app.listen(port, () => {
   console.log(`server is runing in http://localhost:${port}`);
---
+++ router/index.js
@@ -0,0 +1,12 @@
+const Router = require("koa-router");
+const router = new Router();
+
+router.get("/", async (ctx) => {
+  ctx.body = "首页";
+});
+
+router.get("/list", async (ctx) => {
+  ctx.body = "列表页";
+});
+
+module.exports = router;
