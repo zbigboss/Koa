247bcf99708643921186f8ed8199374bdb8e72cb
翁俊梓 <1916222162@qq.com>
2022-07-30 17:12:22 +0800

    feat: 将同一个列表页的所有子地址从路由中拆分出来（模块化）

--- router/index.js
+++ router/index.js
@@ -1,12 +1,14 @@
 const Router = require("koa-router");
 const router = new Router();
+const list = require("./list");
 
 router.get("/", async (ctx) => {
   ctx.body = "首页";
 });
 
-router.get("/list", async (ctx) => {
-  ctx.body = "列表页";
-});
+// router.get("/list", async (ctx) => {
+//   ctx.body = "列表页";
+// });
+router.use("/list", list.routes(), list.allowedMethods()); // NOTE: 这里不要忘了在use要加上你要调用子地址的父级地址，否则不生效（坑）
 
 module.exports = router;
